7c343ceaa9951445cdfe88507d7b786a45f22b1e
alanrich <alan@alanrich.dev>
2024-10-09 13:40:50 +0200

    add recommendations section to personal page

--- src/components/PersonalInterests.jsx
+++ src/components/PersonalInterests.jsx
@@ -40,7 +40,7 @@ function PersonalInterests() {
     "Byzantine History, fascinated by its mysterious allure.",
     "Exploring Roman Ruins, my profile photo is from Herculaneum.",
     "French Comedy, currently addicted to Un Gars, une fille.",
-    "Wine, Côte du Roussilon is my favorite appellation",
+    "Wine, Côte du Roussilon is my favorite appellation.",
   ];
 
   // French Interests
@@ -54,6 +54,72 @@ function PersonalInterests() {
     "Exploration des Ruines Romaines, ma photo de profil vient d'Herculanum.",
     "Comédie Française, actuellement accro à Un Gars, une fille.",
     "Le Vin, Côte du Roussillon est mon appellation préférée.",
+  ];
+
+  // YouTube Channels and Great Courses Recommendations
+  const recommendations = [
+    {
+      img: "https://i1.ytimg.com/vi/Rc0K4WhNOvY/hqdefault.jpg",
+      title: "The Great Minds of the Western Intellectual Tradition",
+      url: "https://www.youtube.com/@dr.michaelsugrue",
+    },
+    {
+      img: "https://secureimages.teach12.com/tgc/images/m2/courses/high/463.jpg",
+      title: "Plato, Socrates, and the Dialogues",
+      url: "https://www.thegreatcourses.com/courses/plato-socrates-and-the-dialogues",
+    },
+    {
+      img: "https://www.thoughtco.com/thmb/jnCNuKybG4olQgB-z2SsX7LKEsA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-587491038-5b3162470e23d90036a05408.jpg",
+      title: "The Ottoman Empire",
+      url: "https://www.thegreatcourses.com/courses/the-ottoman-empire",
+    },
+
+    {
+      img: "https://yt3.ggpht.com/5Kngq4vxIAdfM-ekLYIq9RHWSs3KdJhx1w3WjVaOcwVnFfnkTd04rIxeYyTc28K8KicI8J-kU_U=s176-c-k-c0x00ffffff-no-rj-mo",
+      title: "Arte Documentaire",
+      url: "https://www.youtube.com/watch?v=iJd5CJMJp7A",
+    },
+
+    {
+      img: "https://yt3.googleusercontent.com/nrkCjT2S0wgFLymgDvUYF6DEXS_lpN99c499kTQm6YTIkSN_FthRf9JIQHg7OMI8SKDU3Et0JA=s160-c-k-c0x00ffffff-no-rj",
+      title: "The Rest is History",
+      url: "https://www.youtube.com/channel/UCUYK0BJZF3yNb2fw1EdAXUQ",
+    },
+    {
+      img: "https://i.ytimg.com/vi/pl52AsS-j1c/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC7gu8IAwstrE2BaHwFXiPuo4W3DQ",
+      title: "Books That Have Made History",
+      url: "https://www.youtube.com/playlist?list=PLFBPgxvkodCqK3LDnp1HcdyGJ9wTh8Y_r",
+    },
+    {
+      img: "https://medias.histoire-et-civilisations.com/api/v1/images/view/5f71d657d286c278077e1da4/width_1000/image.jpg",
+      title: "The Story of American Freedom",
+      url: "https://www.youtube.com/watch?v=fqgQ1B2Jjz0&list=PLdSDzmaJo0wlbIW2AXqWbbuh8KhrzyZ7I&index=1",
+    },
+    {
+      img: "https://yt3.googleusercontent.com/ytc/AIdro_l_DnztzuTluEh4JaE7eENqzeOdqs6aUxgkb7C4kdpWnY8=s160-c-k-c0x00ffffff-no-rj",
+      title: "Un Gars, une fille",
+      url: "https://www.youtube.com/@UngarsunefilleOfficiel",
+    },
+    {
+      img: "https://i.ytimg.com/vi/sq5JhW55uGI/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB3gOAAugCigIMCAAQARhlIGUoZTAP&rs=AOn4CLATiu4Gy4ywn6SUxosDZKY_s_fV-Q",
+      title: "The Lives of Famous Greeks & Romans",
+      url: "https://www.youtube.com/@iandespalo2949/videos",
+    },
+    {
+      img: "https://ichef.bbci.co.uk/images/ic/1200x675/p0g66bwh.jpg",
+      title: "I, Claudius",
+      url: "https://www.youtube.com/watch?v=Z7XRX1UBooQ&list=PLlwzDYw6zp2D6DxLR8zGz1i_7764DYSVQ",
+    },
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/1/10/Alcibades_being_taught_by_Socrates%2C_Fran%C3%A7ois-Andr%C3%A9_Vincent.jpg",
+      title: "Alcibiades",
+      url: "https://www.youtube.com/watch?v=APWamijL6xc",
+    },
+    {
+      img: "https://i.ytimg.com/vi/HTm2kEAfo8o/maxresdefault.jpg",
+      title: "The Brandenburg Concertos",
+      url: "https://www.youtube.com/watch?v=NCPM8DEsvmc",
+    },
   ];
 
   const images = [
@@ -207,6 +273,38 @@ function PersonalInterests() {
                 )}
               </Box>
             </Modal>
+
+            {/* Recommendations Section */}
+            <Typography variant="h4" component="h3" gutterBottom>
+              Recommendations
+            </Typography>
+            <ImageList variant="masonry" cols={3} gap={8}>
+              {recommendations.map((rec, index) => (
+                <ImageListItem key={index} sx={{ cursor: "pointer" }}>
+                  <a href={rec.url} target="_blank" rel="noopener noreferrer">
+                    <img
+                      src={rec.img}
+                      alt={rec.title}
+                      style={{
+                        width: "100%",
+                        height: "180px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </a>
+                  <Typography
+                    variant="body2"
+                    component="div"
+                    sx={{
+                      height: "40px",
+                      display: "flex",
+                    }}
+                  >
+                    {rec.title}
+                  </Typography>
+                </ImageListItem>
+              ))}
+            </ImageList>
           </>
         ) : (
           <>
@@ -326,6 +424,37 @@ function PersonalInterests() {
                 )}
               </Box>
             </Modal>
+            {/* Recommendations Section */}
+            <Typography variant="h4" component="h3" gutterBottom>
+              Recommendations
+            </Typography>
+            <ImageList variant="masonry" cols={3} gap={8}>
+              {recommendations.map((rec, index) => (
+                <ImageListItem key={index} sx={{ cursor: "pointer" }}>
+                  <a href={rec.url} target="_blank" rel="noopener noreferrer">
+                    <img
+                      src={rec.img}
+                      alt={rec.title}
+                      style={{
+                        width: "100%",
+                        height: "200px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </a>
+                  <Typography
+                    variant="body2"
+                    component="div"
+                    sx={{
+                      height: "40px",
+                      display: "flex",
+                    }}
+                  >
+                    {rec.title}
+                  </Typography>
+                </ImageListItem>
+              ))}
+            </ImageList>
           </>
         )}
       </Box>
