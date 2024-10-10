49572eebfaf2e7cff330f4f731a299871e923fc8
alanrich <alan@alanrich.dev>
2024-10-10 14:55:53 +0200

    refactor personal interest content to their own files

---
+++ src/assets/recommendations.js
@@ -0,0 +1,49 @@
+const recommendations = [
+  {
+    img: "https://i1.ytimg.com/vi/Rc0K4WhNOvY/hqdefault.jpg",
+    title: "The Great Minds of the Western Intellectual Tradition",
+    url: "https://www.youtube.com/@dr.michaelsugrue",
+  },
+  {
+    img: "https://secureimages.teach12.com/tgc/images/m2/courses/high/463.jpg",
+    title: "Plato, Socrates, and the Dialogues",
+    url: "https://www.thegreatcourses.com/courses/plato-socrates-and-the-dialogues",
+  },
+  {
+    img: "https://www.thoughtco.com/thmb/jnCNuKybG4olQgB-z2SsX7LKEsA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-587491038-5b3162470e23d90036a05408.jpg",
+    title: "The Ottoman Empire",
+    url: "https://www.thegreatcourses.com/courses/the-ottoman-empire",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/nrkCjT2S0wgFLymgDvUYF6DEXS_lpN99c499kTQm6YTIkSN_FthRf9JIQHg7OMI8SKDU3Et0JA=s160-c-k-c0x00ffffff-no-rj",
+    title: "The Rest is History",
+    url: "https://www.youtube.com/channel/UCUYK0BJZF3yNb2fw1EdAXUQ",
+  },
+  {
+    img: "https://i.ytimg.com/vi/pl52AsS-j1c/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC7gu8IAwstrE2BaHwFXiPuo4W3DQ",
+    title: "Books That Have Made History",
+    url: "https://www.youtube.com/playlist?list=PLFBPgxvkodCqK3LDnp1HcdyGJ9wTh8Y_r",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/ytc/AIdro_l_DnztzuTluEh4JaE7eENqzeOdqs6aUxgkb7C4kdpWnY8=s160-c-k-c0x00ffffff-no-rj",
+    title: "Un Gars, une fille",
+    url: "https://www.youtube.com/@UngarsunefilleOfficiel",
+  },
+  {
+    img: "https://i.ytimg.com/vi/sq5JhW55uGI/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB3gOAAugCigIMCAAQARhlIGUoZTAP&rs=AOn4CLATiu4Gy4ywn6SUxosDZKY_s_fV-Q",
+    title: "The Lives of Famous Greeks & Romans",
+    url: "https://www.youtube.com/@iandespalo2949/videos",
+  },
+  {
+    img: "https://ichef.bbci.co.uk/images/ic/1200x675/p0g66bwh.jpg",
+    title: "I, Claudius",
+    url: "https://www.youtube.com/watch?v=Z7XRX1UBooQ&list=PLlwzDYw6zp2D6DxLR8zGz1i_7764DYSVQ",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/5Kngq4vxIAdfM-ekLYIq9RHWSs3KdJhx1w3WjVaOcwVnFfnkTd04rIxeYyTc28K8KicI8J-kU_U=s160-c-k-c0x00ffffff-no-rj",
+    title: "Arte",
+    url: "https://www.youtube.com/watch?v=iJd5CJMJp7A",
+  },
+];
+
+export default recommendations;
