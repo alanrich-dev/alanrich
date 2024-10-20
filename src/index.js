a8e9ff4c284de49561bea4c6fbe90d3af55a5502
alanrich <alan@alanrich.dev>
2024-10-21 00:22:11 +0200

    fix: import appropriate font weight for Inter font from google

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import theme from "./theme";
 import "@fontsource/inter"; // Defaults to weight 400
+import "@fontsource/inter/700.css"; // Bold
+import "@fontsource/inter/900.css"; // Extra Bold
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
