f5a26e47641a0ee5e183b9dc0ec507ca2b38ae96
alanrich <alan@alanrich.dev>
2024-10-21 00:57:33 +0200

    make personal interests page more personal

--- src/assets/content.js
+++ src/assets/content.js
@@ -2,24 +2,23 @@ const content = {
   en: {
     heading: "How I spend my free time",
     subheading: "Personal Interests",
-    introduction: `The Romans had a concept of leisure that resonates well with my 
-      inclinations regarding how I spend my time beyond works, <i>Otium cum Dignitate</i>,
-      leisure with dignity, the notion that leisure well spent is leisure that
-      enriches the mind and soul. For me, the most enjoyable moments are those
-      spent either laughing or furthering my understanding of the world and how it
-      came to be as it is. My alone time is usually occupied with reading history,
-      pondering philosophy, indulging in comedy or performing vigorous
-      exercise. Being a well rounded, cultured and thoughtful individual
-      is a privilege of education that I fully embrace and strive to
-      constantly develop. I recently took a sabbatical from work to do a
-      grand tour of Europe. Whilst inter-railing, I visited 56 cities,
-      where I viewed stunning works of art and civilization, spent
-      nights under the stars (and the midnight sun) and walked in the
-      footsteps of historical figures who have been transfixed in my
-      imagination since I was an adolescent studying Latin and ancient
-      Greek. It was a rejuvenating experience, a well needed break from
-      the grind, and it revitalized my passion for my trade, software
-      development.`,
+    introduction: `<p>
+  The Romans had a concept of leisure that resonates well with my inclinations, <i>Otium cum Dignitate</i>, leisure with dignity, the notion that leisure well spent is leisure that enriches the mind and soul. My most enjoyable moments are those spent either laughing or furthering my understanding of the world and how it came to be as it is. My personal time is usually occupied with reading history, pondering philosophy, indulging in comedy, or performing vigorous exercise. Being a well-rounded, cultured, and thoughtful individual is a privilege of education that I fully embrace and strive to constantly develop.
+</p>
+
+<p>
+  I recently took a sabbatical from work to do a grand tour of Europe. Whilst inter-railing, I visited 56 cities, where I viewed stunning works of art and civilization, spent nights under the stars (and the midnight sun), and walked in the footsteps of historical figures who have been transfixed in my imagination since I was an adolescent studying Latin and ancient Greek. It was a rejuvenating experience, a well-needed break from the grind, and it revitalized my passion for my trade, software development.
+</p>
+
+<p>
+  When I returned to France, I was inspired to merge my love of culture with my passion for coding. This inspiration led me to create <a href="https://www.decouvrirpatrimoine.fr" target="_blank" rel="noopener noreferrer" style="color: blue; text-decoration: underline;">Decouvrirpatrimoine.fr</a>, a website designed to make government data on French heritage more accessible and engaging. Although it is still a work in progress, it remains my passion project.
+</p>
+
+<p>
+  I deeply admire French heritage and its profound contributions to the Western intellectual tradition. Utilizing my skills to enhance the accessibility of this heritage for curious minds brings me great joy. Additionally, this project serves as a positive feedback loop for my professional development as a software developer. It motivates me to expand my technical expertise and broaden my competencies, all while pursuing a passion that aligns with my personal interests.
+</p>
+`,
+
     interestsTitle: "Some of my passions",
     photoGalleryTitle: "2024 Highlights",
     recommendationsTitle: "What I like to watch",
@@ -27,27 +26,22 @@ const content = {
   fr: {
     heading: "Mes Passe-temps",
     subheading: "Intérêts Personnels",
-    introduction: `Les Romains avaient une conception des loisirs qui résonne 
-      bien avec mes inclinations quant à la manière dont je passe mon temps hors 
-      du travail. <i>Otium cum Dignitate</i>, le loisir avec dignité,
-      l'idée que le loisir bien utilisé est celui qui enrichit l'esprit
-      et l'âme. Pour moi, les moments les plus agréables sont ceux
-      passés à rire ou à approfondir ma compréhension du monde et de la
-      manière dont il est devenu ce qu'il est aujourd'hui. Mon temps
-      seul est généralement occupé par la lecture d'histoire, la
-      réflexion philosophique, l'indulgence dans la comédie ou
-      l'exercice physique intense. Être un individu cultivé, réfléchi et
-      polyvalent est un privilège de l'éducation que j'embrasse
-      pleinement et que je m'efforce constamment de développer. J'ai
-      récemment pris un congé sabbatique pour faire un grand tour de
-      l'Europe. Pendant mon voyage inter-railing, j'ai visité 56 villes,
-      où j'ai admiré des œuvres d'art et de civilisation magnifiques,
-      passé des nuits sous les étoiles (et le soleil de minuit) et
-      marché dans les traces de figures historiques qui ont captivé mon
-      imagination depuis que j'étais adolescent, étudiant le latin et le
-      grec ancien. Ce fut une expérience revigorante, une pause bien
-      méritée du quotidien, qui a ravivé ma passion pour mon métier, le
-      développement logiciel.`,
+    introduction: `<p>
+  Les Romains avaient un concept de loisirs qui résonne bien avec mes inclinations, <i>Otium cum Dignitate</i>, le loisir avec dignité—la notion que le loisir bien utilisé est un loisir qui enrichit l'esprit et l'âme. Mes moments les plus agréables sont ceux passés à rire ou à approfondir ma compréhension du monde et de son évolution. Mon temps personnel est généralement consacré à la lecture de l'histoire, à la réflexion philosophique, à l'indulgence dans la comédie ou à la pratique d'exercices vigoureux. Être une personne équilibrée, cultivée et réfléchie est un privilège de l'éducation que j'embrasse pleinement et que je m'efforce de développer constamment.
+</p>
+
+<p>
+  J'ai récemment pris un congé sabbatique pour effectuer une grande tournée en Europe. En interrail, j'ai visité 56 villes, où j'ai admiré des œuvres d'art et de civilisation époustouflantes, passé des nuits sous les étoiles (et le soleil de minuit), et marché sur les traces de figures historiques qui ont captivé mon imagination depuis que j'étais adolescent étudiant le latin et le grec ancien. Ce fut une expérience régénérante, une pause bien nécessaire loin de la routine, et cela a revitalisé ma passion pour mon métier, le développement logiciel.
+</p>
+
+<p>
+  À mon retour en France, j'ai été inspiré pour fusionner mon amour de la culture avec ma passion pour le codage. Cette inspiration m'a conduit à créer <a href="https://www.decouvrirpatrimoine.fr" target="_blank" rel="noopener noreferrer" style="color: blue; text-decoration: underline;">Decouvrirpatrimoine.fr</a>, un site web conçu pour rendre les données gouvernementales sur le patrimoine français plus accessibles et attrayantes. Bien qu'il soit encore en cours de développement, il demeure mon projet passion.
+</p>
+
+<p>
+  J'admire profondément le patrimoine français et ses contributions profondes à la tradition intellectuelle occidentale. Utiliser mes compétences pour améliorer l'accessibilité de ce patrimoine pour les esprits curieux me procure une grande joie. De plus, ce projet sert de boucle de rétroaction positive pour mon développement professionnel en tant que développeur logiciel. Il me motive à élargir mon expertise technique et à accroître mes compétences, tout en poursuivant une passion qui s'aligne avec mes intérêts personnels.
+</p>
+`,
     interestsTitle: "Quelques-unes de mes passions",
     photoGalleryTitle: "Les moments forts de 2024",
     recommendationsTitle: "Ce que j'aime regarder",
